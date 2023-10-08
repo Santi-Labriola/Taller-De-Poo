7154b2da1a8a54cf6001e4605017713c57c40d9c
Matias <Matias@DESKTOP-TS2NHON>
2023-10-08 20:39:48 -0300

    Esta clase raris porq si

--- tallerPOO/src/main/java/com/mycompany/tallerpoo/FuncionarioAdministrativo.java
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/FuncionarioAdministrativo.java
@@ -1,9 +1,8 @@
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.tallerpoo;
 
 /**
  *
@@ -24,7 +23,3 @@ public class FuncionarioAdministrativo  {
         this.nombre = nombre;
     }
 }
-
-
-
-
