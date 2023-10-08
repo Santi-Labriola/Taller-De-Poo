73972be6e71c2b515bf7a56762f933f61076c64a
Lucas <Lucas@Lucas-PC>
2023-10-08 20:48:43 -0300

    IMPORTANTE: Se creo una lista de los pacientes que aun no estan atendidos, asi los medicos pueden elegir a que paciente atender y quitar a los que se vayan atendiendo con los metodos especificados.

--- tallerPOO/src/main/java/com/mycompany/tallerpoo/Medico.java
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/Medico.java
@@ -2,14 +2,16 @@
 
 package com.mycompany.tallerpoo;
 import java.time.*;
+import java.util.*;
 
 /**
  *
  * @author Lucas
  */
 public class Medico extends Persona{
-        private int matricula;
-
+    private int matricula;
+    public static ArrayList<Paciente> listaPacientes=new ArrayList<>(); //lista que almacenara pacientes no atendidos.
+    
     public Medico(String nombre, LocalDate fechaNacimiento, String domicilio, 
                   int documento, int telefoFijo, int telefonoCelular, 
                   String estadoCivil, String correoElectronico, int matricula) {
@@ -26,4 +28,17 @@ public class Medico extends Persona{
         this.matricula = matricula;
     }
     
+    
+    //Atender paciente quita de la lista de pacientes para atender, asi los otros
+    //medicos ya no pueden verlo
+    public void atenderPaciente(Paciente paciente){
+        Medico.listaPacientes.remove(paciente); 
+    }
+    
+    //hacer admision de pacientes que llegan al hospital, se los almacena en una
+    //lista la cual contiene todos los pacientes en espera.
+    //los medico pueden ver esta lista y atender (eliminar de lista) a pacientes.
+    public void hacerAdmision(Paciente paciente){
+        Medico.listaPacientes.add(paciente);
+    }
 }
