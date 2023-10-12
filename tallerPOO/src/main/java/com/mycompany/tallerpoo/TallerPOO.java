8aea8befd0f1979e95afab42abbc285a16a95ad7
Matías <Matas@DESKTOP-C1PJUQL.bbrouter>
2023-10-12 10:01:15 -0300

    Se creo el archivo?

--- tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
@@ -14,13 +14,14 @@ public class TallerPOO {
 
     public static void main(String[] args) {
         //System.out.println("Hello ksdaodkao!");
-        //crearArchivo("archivo\\Hola.txt");//crea un archivo en el proyecto de net
+        crearArchivo("Triage.txt");//crea un archivo en el proyecto de net
         //crearArchivo("C:\\JavaArchivos\\hola.txt");//crea un archivo tipo txt en el disco c
         
         //escribirArchivo("archivo\\Hola.txt", "andara?");
         //escribirArchivo("archivo\\Hola.txt", "Felicidades, anda");asi se le puede subir archivos
         
         //leerArchivo("archivo\\Hola.txt"); //bueno y asi lees todo el contenido de tu archivo- metodo(nombreDeCarpeta, nombredearchivo)
+        
         
         
     }
