9f3e69ddff6ddd076ada529c6518a0907d438c2d
ValentinDelfin <147278466+ValentinDelfin@users.noreply.github.com>
2023-10-13 22:02:42 -0300

    Add files via upload
    Se le agregaron un par de JFrame a la interfaz

---
+++ TallerPoo_Valentin_13102023/src/main/java/PantallaMain/Main.java
@@ -0,0 +1,15 @@
+
+package PantallaMain;
+import Interfaz.PantallaLogin;
+public class Main {
+    
+    public static void main(String [] args){  
+    PantallaLogin login = new PantallaLogin();
+    login.setVisible(true);
+    login.setLocationRelativeTo(null);
+    
+    
+    
+    
+    }
+}
---
+++ TallerPoo_Valentin_13102023/src/main/java/com/mycompany/tallerpoo/Box.java
@@ -0,0 +1,35 @@
+
+package com.mycompany.tallerpoo;
+
+
+/**
+ *
+ * @author Lucas
+ */
+public class Box {
+    private int numero;
+    private boolean ocupado;
+
+    public Box(int numero, boolean ocupado) {
+        this.numero = numero;
+        this.ocupado = ocupado;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+    
+    
+}
