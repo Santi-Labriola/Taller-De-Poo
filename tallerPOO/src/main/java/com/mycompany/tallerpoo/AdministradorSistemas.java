39c4c3d09892d703f1c1e9abde074a3794ee96d4
Matias <Matias@DESKTOP-TS2NHON>
2023-10-14 16:00:01 -0300

    Error en el metodo

---
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/AdministradorSistemas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerpoo;
+
+public class AdministradorSistemas extends FuncionarioGeneral {
+    public ArrayList<Rol> rol;
+    
+    public void asignarRol(Persona persona, Sector sector){
+        if (sector == sectorMedico){
+            persona = new Medico ();
+        }
+    }
+}
