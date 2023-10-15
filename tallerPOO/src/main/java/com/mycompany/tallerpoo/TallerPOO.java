3792908e8d5282b30f21f3142d8a840ffb7f0787
Lucas <Lucas@Lucas-PC>
2023-10-15 19:42:12 -0300

    Se modifico el main

--- tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.tallerpoo;
 
 /**
@@ -9,20 +5,31 @@ package com.mycompany.tallerpoo;
  * @author 54345
  */
 import static com.mycompany.tallerpoo.ManejoArchivos.*;
-import static com.mycompany.tallerpoo.Triage.*;
+
+import java.time.*;
+import java.util.ArrayList;
+import InterfazUsuario.BuscarEstFecha;
+
+
 public class TallerPOO {
 
     public static void main(String[] args) {
         //System.out.println("Hello ksdaodkao!");
-        crearArchivo("folder\\Triage.txt");//crea un archivo en el proyecto de net
+        //crearArchivo("folder\\Triage.txt");//crea un archivo en el proyecto de net
         //crearArchivo("C:\\JavaArchivos\\hola.txt");//crea un archivo tipo txt en el disco c
         
-      escribirArchivo("folder\\Triage.txt", "andara?");
+        //escribirArchivo("folder\\Triage.txt", "andara?");
         //escribirArchivo("archivo\\Hola.txt", "Felicidades, anda");asi se le puede subir archivos
         
         //leerArchivo("archivo\\Hola.txt"); //bueno y asi lees todo el contenido de tu archivo- metodo(nombreDeCarpeta, nombredearchivo)
         
         
-        
+        Paciente paci=new Paciente("Lucas Nuñez",LocalDate.of(2022,1,1),"Alegria 123",44092390,4438354,3454343,"Soltero","lucasmagiconunez@gmail.com","Papa de lucas");
+        ArrayList lista=ResultadoEstudios.obtenerResultadoEstudios(LocalDate.of(2023,5,10));
+
+        System.out.println(lista);
+        BuscarEstFecha abc=new BuscarEstFecha();
+        abc.setVisible(true);
+        abc.setLocationRelativeTo(null);
     }
 }
