e61c11d3d78f938bd6b051568b84be844a6130f6
Lucastnunez <>
2023-10-20 23:29:39 -0300

    Se agrego la clase ListaResEstudios

---
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/ListaResEstudios.java
@@ -0,0 +1,98 @@
+
+package com.mycompany.tallerpoo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ListaResEstudios {
+    protected static ArrayList <ResultadoEstudios> resultados;
+
+    public ListaResEstudios(ArrayList<ResultadoEstudios> resultados) {
+        this.resultados = resultados;
+    }
+
+    public ListaResEstudios() {
+        this.resultados=new ArrayList <ResultadoEstudios>();
+    }
+
+    public ArrayList<ResultadoEstudios> getResultados() {
+        return resultados;
+    }
+
+    public void setResultados(ArrayList<ResultadoEstudios> resultados) {
+        this.resultados = resultados;
+    }
+    
+    public void agregar(ResultadoEstudios resu){
+        resultados.add(resu);
+    }
+    
+    public void leer(String archivo){
+        leerInterno(archivo,null);
+    }
+    
+    public void leer(String archivo, ListaPacientes listapaci){
+        leerInterno(archivo,listapaci);
+    }
+        
+    private void leerInterno(String archivo, ListaPacientes listapaci){
+        
+        try {
+            BufferedReader reader= new BufferedReader (new FileReader(archivo));
+            String linea = reader.readLine();
+            
+            while (linea!=null){
+                String[] split=linea.split(",");//splitea la linea
+                String[] splitFecha=split[1].split("/");//splitea la fecha 
+                String[] splitHora=split[2].split(":");//splitea la hora
+                
+                ResultadoEstudios resu=new ResultadoEstudios();
+                
+                //se inicializar la fecha y hora
+                LocalDate fecha= LocalDate.of(Integer.parseInt(splitFecha[2]),
+                                                Integer.parseInt(splitFecha[1]),
+                                                Integer.parseInt(splitFecha[0]));
+                
+                LocalTime hora= LocalTime.of(Integer.parseInt(splitHora[0]),
+                                                Integer.parseInt(splitHora[1]),
+                                                Integer.parseInt(splitHora[2]));
+                
+                resu.setFecha(fecha);
+                resu.setHora(hora);
+                
+                resu.setTipoDeEstudio(split[3]);
+                resu.setInformeDeEstudio(split[4]);
+               
+                /*si se paso la lista de pacientes por parametro, se busca por dni al paciente
+                y hace un setPaciente.
+                */
+                if (listapaci!=null){            
+                    Paciente paci=listapaci.getPorDni(Integer.parseInt(split[0]));
+                    resu.setPaciente(paci);
+                    
+                    //si el paciente existe, se añade este resultado de estudio a la lista del paciente.
+                    if (paci!=null){
+                        paci.agregarEstudio(resu);
+                    }
+                }
+                this.agregar(resu);
+                linea = reader.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
