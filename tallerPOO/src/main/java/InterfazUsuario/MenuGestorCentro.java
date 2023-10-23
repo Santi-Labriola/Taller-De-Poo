91301239f2028020472dd601b587ed65d41c850b
Lucastnunez <>
2023-10-23 12:43:43 -0300

    Se adaptaron las interfaces.

--- tallerPOO/src/main/java/InterfazUsuario/MenuGestorCentro.java
+++ tallerPOO/src/main/java/InterfazUsuario/MenuGestorCentro.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package igu;
+package InterfazUsuario;
 
-import igu.*;
+import InterfazUsuario.Op1GestorCentro;
+import com.mycompany.tallerpoo.*;
 /**
  *
  * @author 54345
--- tallerPOO/src/main/java/InterfazUsuario/Op1GestorCentro.java
+++ tallerPOO/src/main/java/InterfazUsuario/Op1GestorCentro.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package igu;
+package InterfazUsuario;
 import java.time.LocalDate;
-import logica.RegistroMedico;
+import com.mycompany.tallerpoo.*;
 
 /**
  *
