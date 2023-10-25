de16ab774f0562ed73f67cb140291ad4c6552ee1
Lucastnunez <>
2023-10-25 01:02:40 -0300

    Se modifico el triage

--- tallerPOO/src/main/java/com/mycompany/tallerpoo/Triage.java
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/Triage.java
@@ -1,8 +1,4 @@
-/*
- *pr
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.tallerpoo;
 
 import java.io.BufferedReader;
