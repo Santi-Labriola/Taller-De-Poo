b258e1963970695ebff5bdce65599f70da9e02fa
Matias <Matias@DESKTOP-TS2NHON.bbrouter>
2023-10-27 18:06:31 -0300

    ASignacion box por la mitad

--- tallerPOO/src/main/java/com/mycompany/tallerpoo/ListaAsignacionBox.java
+++ tallerPOO/src/main/java/com/mycompany/tallerpoo/ListaAsignacionBox.java
@@ -4,16 +4,36 @@ package com.mycompany.tallerpoo;
 import java.util.ArrayList;
 
 /**
- *
+ * Esta clase es una colección que almacena instancias de la clase AsignacionBox.
+ * Nos permite agregar asignaciones de box a la lista y proporciona métodos para acceder y gestionar
+ * estas asignaciones.
  * @author Usuario
  */
-public class ListaAsignacionBox {
-    ArrayList<AsignacionBox> asignaciones;
 
+public class ListaAsignacionBox {
+    /**
+     * Esta clase es una colección que guarda instancias de la clase AsignacionBox.
+     * Permite agregar asignaciones de box a la lista y proporciona métodos para acceder y gestionar
+     * estas asignaciones.
+     */    
+    private ArrayList<AsignacionBox> asignaciones;
+
+    /**
+     * Constructor de la clase que crea una lista vacía de asignaciones de box.
+     */
+    
     public ListaAsignacionBox() {
         this.asignaciones=new ArrayList<AsignacionBox>();
     }
 
+    
+    /**
+     * Constructor de la clase ListaAsignacionBox que permite inicializar la lista con una lista
+     * existente de asignaciones de box.
+     *
+     * @param asignaciones La lista de asignaciones de box para inicializar.
+     */
+    
     public ListaAsignacionBox(ArrayList<AsignacionBox> asignaciones) {
         this.asignaciones = asignaciones;
     }
